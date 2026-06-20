e7f1ec2cff70ea8fa40c035db7417b442fe1bb74
LiherRiosRuiz <liherios@proton.me>
2026-06-21 01:29:38 +0200

    feat(api): Form Request UpdateProfileRequest.php

---
+++ projects/web/api-laravel/app/Http/Requests/UpdateProfileRequest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateProfileRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'name' => 'sometimes|string|max:120',
+            'bio' => 'nullable|string|max:500',
+            'avatar_url' => 'nullable|string',
+            'transformation_goal' => 'nullable|array',
+            'privacy_settings' => 'nullable|array',
+        ];
+    }
+}
